8cb11717aa58633d5cac0f76822046753e08db88
kyuminlee <tidls93@naver.com>
2024-04-25 14:17:20 +0900

    Initial Commit

---
+++ SwiftUINavigationController/SwiftUINavigationControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINavigationControllerApp.swift
+//  SwiftUINavigationController
+//
+//  Created by kyuminlee on 4/25/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINavigationControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
